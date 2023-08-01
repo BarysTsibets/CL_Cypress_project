8d62a947d91daa8befc19770b6aa1c1e258dd392
Barys Tsibets <barystsibets@gmail.com>
2023-07-31 18:22:17 -0700

    refactoring

--- cypress/e2e/pages/login_page.js
+++ cypress/e2e/pages/login_page.js
@@ -1,21 +1,25 @@
 export class LoginPage{
 
+    loginpage_email_textbox = '#inputEmailHandle'
+    loginpage_password_textbox = '#inputPassword'
+    loginpage_login_button = '#login'
+
     goToMyAccount(){
         cy.contains('my account').click()
     }
 
     enterUserName(email){
-        cy.get('#inputEmailHandle').type(email)
+        cy.get(this.loginpage_email_textbox).type(email)
         //barystsibets@gmail.com
     }
 
     enterpassword(paswd){
-        cy.get('#inputPassword').type(paswd)
+        cy.get(this.loginpage_password_textbox).type(paswd)
         //361337aB!
     }
 
     clickLogIn(){
-        cy.get('#login').click()
+        cy.get(this.loginpage_login_button).click()
     }
 
 }
--- cypress/e2e/pomdemo.cy.js
+++ cypress/e2e/pomdemo.cy.js
@@ -3,22 +3,26 @@ import {LoginPage} from "./pages/login_page"
 const loginPage = new LoginPage()
 
 
-it('test description', function(){
+describe('test SuitName', function(){
+
+    it('test description', function(){
     
-    // Open Main Page of CL and click "My account"
-    cy.visit('https://losangeles.craigslist.org')
-
-
-    loginPage.goToMyAccount()
-    loginPage.enterUserName('barystsibets@gmail.com')
-    loginPage.enterpassword('361337aB!')
-    loginPage.clickLogIn()
-
-
-    // cy.contains('my account').click()
-    // Fill out LogIn text boxes
-    // cy.get('#inputEmailHandle').type('barystsibets@gmail.com')
-    // cy.get('#inputPassword').type('361337aB!')
-    // cy.get('#login').click()
+        // Open Main Page of CL and click "My account"
+        cy.visit('https://losangeles.craigslist.org')
+    
+        loginPage.goToMyAccount()
+        loginPage.enterUserName('barystsibets@gmail.com')
+        loginPage.enterpassword('361337aB!')
+        loginPage.clickLogIn()
+    
+        // cy.contains('my account').click()
+        // Fill out LogIn text boxes
+        // cy.get('#inputEmailHandle').type('barystsibets@gmail.com')
+        // cy.get('#inputPassword').type('361337aB!')
+        // cy.get('#login').click()
+    
+    })
 
 })
+
+
