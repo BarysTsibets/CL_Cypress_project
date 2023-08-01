55c30eb307edf290b72b832455ea177aa4d67291
Barys Tsibets <barystsibets@gmail.com>
2023-07-31 18:05:59 -0700

    refactoring

--- cypress/e2e/pomdemo.cy.js
+++ cypress/e2e/pomdemo.cy.js
@@ -1,15 +1,24 @@
-import LoginPage from "./pages/login_page"
+import {LoginPage} from "./pages/login_page"
+
+const loginPage = new LoginPage()
+
 
 it('test description', function(){
+    
     // Open Main Page of CL and click "My account"
     cy.visit('https://losangeles.craigslist.org')
-    cy.contains('my account').click()
 
+
+    loginPage.goToMyAccount()
+    loginPage.enterUserName()
+    loginPage.enterpassword()
+    loginPage.clickLogIn()
+
+
+    // cy.contains('my account').click()
     // Fill out LogIn text boxes
-    cy.get('#inputEmailHandle').type('barystsibets@gmail.com')
-    cy.get('#inputPassword').type('361337aB!')
-    cy.get('#login').click()
+    // cy.get('#inputEmailHandle').type('barystsibets@gmail.com')
+    // cy.get('#inputPassword').type('361337aB!')
+    // cy.get('#login').click()
 
-
-    
 })
