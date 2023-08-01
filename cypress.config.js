40673e6f2caa2b2c184a50b711024c13f20ef91a
Barys Tsibets <barystsibets@gmail.com>
2023-07-31 17:08:56 -0700

    refactoring

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
     defaultCommandTimeout: 3000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      // test comment
     },
   },
 });
